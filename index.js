70a1997fcee291549321dc4915283849e46ebcbb
furucrm-nhatvo <nhat.vo@furucrm.com>
2023-09-24 23:19:55 +0700

    'deploy'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  console.log("%cindex.js line:8 req", "color: #007acc;", req);
+  console.log("%cindex.js line:8 req", "color: #007acc;", req.body);
   res.send("Received");
 });
 app.listen(process.env.PORT || 3000);
